79c13c6e5f238d267ff55edbb980c6f7defc97de
tobilobaolajiga <tobilobaolajiga@gmail.com>
2024-02-22 11:12:46 +0100

    new commit

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -9,7 +9,7 @@ export default function Modal({ imgSrc, imgName, imgLocation, getImg }) {
         onClick={getImg}
       >
         <img
-          src="public\x.svg"
+          src="./x.svg"
           alt=""
           width={40}
           height={40}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -53,7 +53,7 @@ export default function SearchBar() {
       <div>
         <div className="bg-slate-200  w-full ">
           <img
-            src="public\searchIcon.svg"
+            src="./searchIcon.svg"
             width="20"
             height="20"
             className="absolute top-[115px] left-12"
